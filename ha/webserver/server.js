dab73949144bb0c6bf1cc79ead180ea65a92d289
stevenewald <stevenewald6@gmail.com>
2023-06-06 10:55:52 -0500

    Added webservice/website

--- ha/webserver/server.js
+++ ha/webserver/server.js
@@ -1,37 +1,119 @@
-const express = require('express');
-const { Pool } = require('pg');
-const bodyParser = require('body-parser');
+const express = require("express");
+const { Pool } = require("pg");
+const bodyParser = require("body-parser");
+const AWS = require("aws-sdk");
 
 const app = express();
 
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
+
 const pool = new Pool({
-  host: '3.15.100.179',
-  user: 'postgres',
-  password: 's@lami',
-  database: 'testdb',
+  host: "3.15.100.179",
+  user: "postgres",
+  password: "s@lami",
+  database: "testdb",
   port: 5000,
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/add', async (req, res) => {
+//admittedly, terrible security practice
+//to have an express endpoint have full ec2 access
+//and a public api to reboot any instance
+//it's fine bc just for the demo, then deleting access keys and cluster
+const instanceMappings = {
+  "node-1": "i-07500046b93073c66",
+  "node-2": "i-0dd289054e0ad6fd3",
+  "node-3": "i-0e1da344b591f45ce",
+  "etcd": "i-05671c346e9be6aee",
+  "haproxy": "i-0b98a2c01e34765eb",
+};
+app.post("/reboot", async (req, res) => {
+  const { instanceId } = req.body;
+  const params = {
+    InstanceIds: [instanceMappings[instanceId]],
+    DryRun: false,
+  };
+
+  try {
+    await ec2.rebootInstances(params).promise();
+    res.status(200).json({
+      status: "success",
+      message: "Instance rebooted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: "error", message: "Instance reboot error" });
+  }
+});
+
+app.get("/status", async (_, res) => {
+  const params = {
+    InstanceIds: Object.values(instanceMappings),
+  };
+
+  try {
+    const data = await ec2.describeInstances(params).promise();
+    const instances = data.Reservations.flatMap((r) => r.Instances);
+    const totalInstances = instances.map((i) => {
+      return {
+        InstanceId: i.InstanceId,
+        State: i.State.Name,
+      };
+    });
+    res.status(200).json({ status: "success", data: totalInstances });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: "error",
+      message: "Could not retrieve instance status",
+    });
+  }
+});
+
+app.post("/add", async (req, res) => {
   const { first, last, date } = req.body;
 
   try {
     const query = {
-      text: 'INSERT INTO names(first_name, last_name, birth_date) VALUES($1, $2, $3)',
+      text:
+        "INSERT INTO names(first_name, last_name, birth_date) VALUES($1, $2, $3)",
       values: [first, last, date],
     };
     await pool.query(query);
 
-    res.status(200).json({ status: 'success', message: 'Data added successfully' });
+    res.status(200).json({
+      status: "success",
+      message: "Data added successfully",
+    });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ status: 'error', message: 'Database error' });
+    res.status(500).json({ status: "error", message: "Database error" });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit);
+    const query = {
+      text: "SELECT * FROM names ORDER BY id DESC LIMIT $1",
+      values: [limit],
+    };
+    const result = await pool.query(query);
+
+    res.status(200).json({ status: "success", data: result.rows });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: "error", data: -1 });
   }
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
--- ha/website/src/index.js
+++ ha/website/src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { React, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const FormComponent = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [date, setDate] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      first: firstName,
+      last: lastName,
+      date: date,
+    };
+    try {
+      const response = await axios.post("http:/ha-cluster.steve.ee:3000", data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const inputStyle = {
+    margin: "10px",
+    padding: "10px",
+    fontSize: "16px",
+  };
+
+  const buttonStyle = {
+    margin: "10px",
+    padding: "10px",
+    backgroundColor: "#007BFF",
+    color: "white",
+    fontSize: "16px",
+    border: "none",
+    cursor: "pointer",
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        placeholder="First Name"
+        style={inputStyle}
+      />
+      <input
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        placeholder="Last Name"
+        style={inputStyle}
+      />
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        style={inputStyle}
+      />
+      <button type="submit" style={buttonStyle}>Submit</button>
+    </form>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <FormComponent />
+  </>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
